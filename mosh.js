b375dbe3e29728bffb38d3e2eb89e2c0fce6fe6d
labeeblabu <labeelabu7012488568@gmail.com>
2021-12-02 11:32:37 +0530

    added

--- mosh.js
+++ mosh.js
@@ -10,14 +10,10 @@ const details={
 // window.localStorage.setItem("Name","jkjhgkjhg")
 localStorage.setItem("Name",details.Name)
 localStorage.setItem("Email",details.Email)
-
-
 let obj1={name:name,email:email}
 let jsonFile=JSON.stringify(obj1)
 }
 localStorage.setItem(`user1`,jsonFile)
-
-
 let user1=JSON.parse(localStorage.getItem(`user1`))
 let li=document.createElement('li')
 let text=document.createTextNode(`${user1.name}:${user1.email}`)
